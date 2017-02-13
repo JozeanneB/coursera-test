cee27dcc0c13d1af1c2dd89590077cddff82a32e
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2017-02-13 02:41:05 -0200

    Correção ; após function. function  functionName () {} var teste = function () {};

--- Lecture6/app.js
+++ Lecture6/app.js
@@ -13,11 +13,11 @@
                 }
                 
                 return totalStringValue;
-            };
+            }
         
             $scope.displayNumeric = function () {
                 var totalNameValue = calculateNumericForString($scope.name); //get the total value
                 $scope.totalValue = totalNameValue;
-            };     
+            };
         });
 })();
